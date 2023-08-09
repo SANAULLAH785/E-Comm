71799429854235113489b75710e7634d43af882f
SANAULLAH785 <108496645+SANAULLAH785@users.noreply.github.com>
2023-08-09 16:38:48 +0500

    resolve route

--- Controllers/sellerControllers.js
+++ Controllers/sellerControllers.js
@@ -1,25 +1,10 @@
-// const { getCollection } = require("../db/connect");
 const Seller = require('../Models/SellerModel');
-const Product=require('../Models/ProductModel');
+const Product = require('../Models/ProductModel');
 const bcrypt = require('bcrypt');
 require("dotenv").config();
 const jwtSecret = process.env.jwtSecret;
 const jwt = require("jsonwebtoken");
-// const { uploadFile, getFileStream } = require('../Middleware/imageupload')
-// const { uploadFile, getFileStream } = require('../Middleware/s3');
 
-// //
-// const multer = require('multer');
-// const fs = require("fs");
-
-// const cloudinary = require('cloudinary').v2; // Import the cloudinary object
-// cloudinary.config({
-//   cloud_name:'dufg9gvng',
-//   api_key:'976463835649686',
-//   api_secret:'UVR2Hbjqz7pP-s26533dmpHTWH4'  });
-
-
-//
 const sellerControllers = {};
 
 sellerControllers.Signup = async (req, res) => {
@@ -30,10 +15,8 @@ sellerControllers.Signup = async (req, res) => {
         if (existingUser) {
             return res.status(400).json({ error: 'Seller with this email already exists' });
         }
-
         const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
-
         const newseller = new Seller({ name, email, password: hashPassword, role: 'seller' });
         await newseller.save();
 
@@ -52,61 +35,37 @@ sellerControllers.Signin = async (req, res) => {
         }
         const isvalidpassword = await bcrypt.compare(password, existingseller.password);
         if (!isvalidpassword) {
-            return res.status(400).json({error:'invalid password'});
+            return res.status(400).json({ error: 'invalid password' });
         }
-        const token=jwt.sign({userid:existingseller._id,username:existingseller.name},jwtSecret);
+        const token = jwt.sign({ userid: existingseller._id, username: existingseller.name }, jwtSecret);
         res.status(200).send(token);
-    }catch(error){
+    } catch (error) {
         res.status(500).send('Failed to login as Seller');
     }
 };
-// sellerControllers.createProduct = async (req, res) => {
-//    const {name,description}=req.body;
-//    const existingproduct=await Product.findOne({name});
-//    if(existingproduct){
-//     res.status(400).send('Product already exist with this name');
-//    }
-//    const newproduct =new Product({name:name,description:description});
-//    await newproduct.save();
-//    res.status(201).send("Product created");
-// };
+
 sellerControllers.createProduct = async (req, res) => {
     const { name, description, quantity, price } = req.body;
-
     try {
         const existingProduct = await Product.findOne({ product_name: name });
-
         if (existingProduct) {
             return res.status(400).send('Product already exists with this name');
         }
-
-        // if (!req.file) {
-        //     return res.status(400).send('No image uploaded');
-        // }
-        // const file = req.file
-
-        // const result = await cloudinary.uploader.upload(req.file.path);
         console.log(req.imageUrl);
-
         const newProduct = new Product({
             product_name: name,
             description: description,
             quantity: quantity,
             price: price,
-            sellerId: req.user.userid, // Assuming you have this information in your authentication middleware
-            
+            sellerId: req.user.userid,
             image_key: req.imageUrl
         });
-        
-
         await newProduct.save();
         return res.status(201).send(newProduct);
-        
     } catch (error) {
         console.error(error);
         return res.status(500).send('Internal Server Error');
     }
 };
-  
 
 module.exports = sellerControllers;
--- routes/seller.js
+++ routes/seller.js
@@ -2,15 +2,7 @@ const auth = require('../Middleware/auth');
 const sellerControllers = require("../Controllers/sellerControllers");
 const { Router } = require("express");
 const router = Router();
-// const imagesaver= require('../Middleware/imageupload');
-// const storage = multer.diskStorage({
-//     destination: "./images/",
-//     filename: (req, file, cb) => {
-//       cb(null, file.fieldname + "-" + Date.now() + file.originalname);
-//     },
-    
-//   });
-//   const upload = multer({ storage });
+
 const imagesave =require('../Middleware/imagesave');
 router.post("/signup", sellerControllers.Signup);
 router.post("/signin", sellerControllers.Signin);
